2305c29ee831050d80c6e9eb263a5a1b83e6d25c
abitshrestha <abitshrestha2079@gmail.com>
2023-09-04 13:52:15 +0545

    ..//

--- front-end/src/Components/InputFields.js
+++ front-end/src/Components/InputFields.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "font-awesome/css/font-awesome.min.css";
 
 const InputField = () => {
   const [username, setUserName] = useState("");
+  const [textEntered, setTextEntered] = useState(false);
   const [user, setUser] = useState(null);
   const valueChanged = (e) => {
     setUserName(e.target.value);
+
+    setTextEntered(!(e.target.value === ""));
   };
   const fetchUserProfile = async () => {
     try {
@@ -27,6 +31,7 @@ const InputField = () => {
         {/* <label>Enter Github Username here</label> */}
         <br />
         <div className="input-group">
+          {!textEntered && <i className="fa fa-search FAS"></i>}
           <input
             type="text"
             onChange={valueChanged}
@@ -34,24 +39,36 @@ const InputField = () => {
             placeholder="enter github username..."
             name="username"
           />
-          <button onClick={fetchUserProfile}>Fetch User</button>
+          <button onClick={fetchUserProfile}>Search</button>
         </div>
-        <div className="generatedContent">
-          {user? (
-            <div className=".user-profile">
+        <div className={`generatedContent ${user ? "dis" : "none"}`}>
+          {user ? (
+            <div className="user-profile">
               <img
                 width={150}
                 src={user.avatar_url}
                 alt={`${user.login}'s avatar`}
-                />
-              <h2>Username:{user.login}</h2>
-              <p className="repo">{user.url}</p>
-              <p>Repository:{user.repos_url}</p>
-              <p>Followers:{user.followers}</p>
-              <p>Following:{user.following}</p>
-              <p>Github:{user.url}</p>
+              />
+              <div className="details">
+                <div className="top">
+                  <div className="names">
+                    <h1 className="name">{user.name}</h1>
+                    <h2 className="username">{user.login}</h2>
+                  </div>
+                  <p className="created">{user.created_at}</p>
+                </div>
+                <p className="bio">{user.bio}</p>
+                <div className="follows">
+                  <p className="repo"><span>Repo</span><br/>{user.public_repos}</p>
+                  <p className="followers"><span>Followers</span><br/>{user.followers}</p>
+                  <p className="following"><span>Following</span><br/>{user.following}</p>
+                </div>
+                <a className="url" href={user.html_url}><i className="fa fa-link"></i>{" "+user.html_url}</a>
+              </div>
             </div>
-          ):'Ops,No Data found!!'}
+          ) : (
+            "No Such User Found"
+          )}
         </div>
       </div>
     </div>
